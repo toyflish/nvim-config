fbec805d980cab23af9fe8ecc42b092a4defcfb9
Kai <mail@kairautenberg.de>
2023-08-26 11:59:02 +0200

    feat: rubocop ale linter

---
+++ lua/plugins/ale.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "dense-analysis/ale",
+    init = function()
+      vim.g.ale_fixers = {
+        ruby = { "rubocop" },
+      }
+      vim.g.ale_fix_on_save = 1
+    end,
+  },
+}
+-- vim.g.ale_fixers = {
+--   typescriptreact = { 'eslint' },
+--   typescript = { 'eslint' },
+--   javascript = { 'eslint' },
+--   go = { 'gofmt' },
+--   ruby = { 'rubocop' },
+--   vue = { 'prettier' },
+--   yaml = { 'prettier' },
+--   json = { 'prettier' },
+-- }
