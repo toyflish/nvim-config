47db40e3355629f8d420b018fafa1ab6c2e3a485
Kai <mail@kairautenberg.de>
2024-05-26 11:49:41 +0200

    config: harpoon with leader key

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,15 +1,76 @@
 return {
-  'ThePrimeagen/harpoon',
-  keys= {
-    {"<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc= "harpoon quick menu" },
-    {"<leader>hx", function() require("harpoon.mark").add_file() end, desc= "harpoon add file" },
-    {"<leader>hn", function() require("harpoon.ui").nav_next() end, desc= "harpoon next file" },
-    {"<leader>hp", function() require("harpoon.ui").nav_prev() end, desc= "harpoon next file" },
-    {"<leader>ha", function() require("harpoon.ui").nav_file(1) end, desc= "harpoon file 1" },
-    {"<leader>hs", function() require("harpoon.ui").nav_file(2) end, desc= "harpoon file 2" },
-    {"<leader>hd", function() require("harpoon.ui").nav_file(3) end, desc= "harpoon file 3" },
-    {"<leader>hf", function() require("harpoon.ui").nav_file(4) end, desc= "harpoon file 4" },
-    {"<leader>hg", function() require("harpoon.ui").nav_file(5) end, desc= "harpoon file 5" },
+  "ThePrimeagen/harpoon",
+  keys = {
+    {
+      "<leader>hh",
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      desc = "harpoon quick menu",
+    },
+    {
+      "<leader>hx",
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      desc = "harpoon add file",
+    },
+    {
+      "<leader>hn",
+      function()
+        require("harpoon.ui").nav_next()
+      end,
+      desc = "harpoon next file",
+    },
+    {
+      "<leader>hp",
+      function()
+        require("harpoon.ui").nav_prev()
+      end,
+      desc = "harpoon next file",
+    },
+    {
+      "<leader>ha",
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      desc = "harpoon file 1",
+    },
+    {
+      "<leader>hs",
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      desc = "harpoon file 2",
+    },
+    {
+      "<leader>hd",
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      desc = "harpoon file 3",
+    },
+    {
+      "<leader>hf",
+      function()
+        require("harpoon.ui").nav_file(4)
+      end,
+      desc = "harpoon file 4",
+    },
+    {
+      "<leader>hg",
+      function()
+        require("harpoon.ui").nav_file(5)
+      end,
+      desc = "harpoon file 5",
+    },
+    {
+      "<leader>hc",
+      function()
+        require("harpoon.mark").clear_all()
+      end,
+      desc = "harpoon clear all",
+    },
   },
   global_settings = {
     -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
@@ -34,6 +95,5 @@ return {
     tabline = false,
     tabline_prefix = "   ",
     tabline_suffix = "   ",
-  }
-
+  },
 }
