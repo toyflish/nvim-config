125599ce44df97a7e80ec14d3a9c7678c245f1ed
Kai <mail@kairautenberg.de>
2025-07-15 11:22:17 +0200

    feat: keymaps toggle inline diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- Toggle inline diagnostics
+local diagnostics_enabled = true
+vim.keymap.set("n", "<leader>id", function()
+  diagnostics_enabled = not diagnostics_enabled
+  vim.diagnostic.config({
+    virtual_text = diagnostics_enabled,
+  })
+end, { desc = "Toggle inline diagnostics" })
